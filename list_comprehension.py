0f320c6b8359a0787c36a1d731dadfe92973be3f
Brian Reina <brianreinaq@gmail.com>
2021-11-15 20:26:30 -0400

    Ajuste de texto

--- list_comprehension.py
+++ list_comprehension.py
@@ -14,10 +14,11 @@ def run():
         
 #    print(square)
 
-#usando list comprehensions
+# usando list comprehensions
     #square = [i**2 for i in range(1, 101) if i % 3 != 0]
     #print(square)
 
+# Challenge
     square = [i for i in range(1, 100000) if i % 4 == 0 and i % 6 == 0 and i % 9 == 0]
     print(square)
 
